817a95c5f0e0fb0314fcce299a001f16e53ccd65
Justin Lee <JustinDellAdam@live.com>
2022-10-24 01:42:11 +0800

    strict mode on

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <React.StrictMode>
+    <React.StrictMode>
         <App/>
-    // </React.StrictMode>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
