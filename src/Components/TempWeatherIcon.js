c80a6ba3802db1169448479b8d365e1e124e9976
Justin Lee <JustinDellAdam@live.com>
2022-10-24 01:29:10 +0800

    fix css

--- src/Components/TempWeatherIcon.js
+++ src/Components/TempWeatherIcon.js
@@ -8,10 +8,10 @@ class TempWeatherIcon extends Component {
     render() {
         return (
             <div className="col-sm-2 row">
-                <div className="d-flex align-items-center justify-content-center mt-5 pt-5 me-5 pe-5">
+                <div className="d-inline-flex align-self-center">
                     <div className={this.props.weather_icon} id="weather_icon"></div>
                 </div>
-                <div className="h1 align-self-end mb-5 pb-5" id="temperature-now">
+                <div className="h1 align-self-end mb-5" id="temperature-now">
                     {this.props.temp}
                     <sup>
                         <small>°C</small>
