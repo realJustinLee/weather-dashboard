87dbe2784af291254235c1135e4957c7926de78f
Justin Lee <JustinDellAdam@live.com>
2022-10-24 13:04:25 +0800

    fix null parsing

--- src/App.js
+++ src/App.js
@@ -87,28 +87,29 @@ class App extends Component {
             })
 
             let forecast = result.daily_forecast;
-            let weatherStr, iconD, iconN = null;
+            let weatherStr, iconDStr, iconNStr = null;
             let forecast_list = node.state.forecast;
             for (let i = 0; i < 3; ++i) {
                 if (forecast[i].cond_txt_d !== forecast[i].cond_txt_n) {
                     weatherStr = forecast[i].cond_txt_d + "转" + forecast[i].cond_txt_n;
-                    iconD = forecast[i].cond_code_d;
+                    iconDStr = forecast[i].cond_code_d.toString();
                     let icon_code_night = forecast[i].cond_code_n;
                     if (icon_code_night === 100 || icon_code_night === 103) {
                         icon_code_night = icon_code_night + "_night";
                     }
-                    iconN = icon_code_night;
+                    iconNStr = icon_code_night.toString();
                 } else {
                     weatherStr = forecast[i].cond_txt_d;
-                    iconD = forecast[i].cond_code_d;
+                    iconDStr = forecast[i].cond_code_d.toString();
                 }
+
                 forecast_list[i] = {
                     weather: (weatherStr).toString(),
                     temp: (forecast[i].tmp_min + "°C / " + forecast[i].tmp_max + "°C").toString(),
                     //"wind": "东风3-4级", TODO 也可能是“东北风微风”，不应该加“级”
                     wind: (forecast[i].wind_dir + forecast[i].wind_sc + "级").toString(),
-                    iconD: (iconD).toString(),
-                    iconN: (iconN).toString(),
+                    iconD: iconNStr,
+                    iconN: iconDStr,
                 };
             }
 
